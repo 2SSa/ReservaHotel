72ac030877a5743fe00d0c6ed64d86345fe1e345
2ssa <panchazo2013@gmail.com>
2017-11-02 18:20:05 -0600

    Versión Inicial

---
+++ mvcReserHotel/Controllers/ReservacionController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using mvcReserHotel.Models;
+
+namespace mvcReserHotel.Controllers
+{
+    public class ReservacionController : Controller
+    {
+        private ctxHotel db = new ctxHotel();
+
+        // GET: Reservacion
+        public ActionResult Index()
+        {
+            var reservacion = db.reservacion.Include(r => r.cliente).Include(r => r.estado).Include(r => r.recepcionista);
+            return View(reservacion.ToList());
+        }
+
+        // GET: Reservacion/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            reservacion reservacion = db.reservacion.Find(id);
+            if (reservacion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reservacion);
+        }
+
+        // GET: Reservacion/Create
+        public ActionResult Create()
+        {
+            ViewBag.cod_cliente = new SelectList(db.cliente, "cod_cliente", "nombres");
+            ViewBag.cod_estado = new SelectList(db.estado, "cod_estado", "nombre");
+            ViewBag.cod_usuario = new SelectList(db.recepcionista, "cod_usuario", "nombres");
+            return View();
+        }
+
+        // POST: Reservacion/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "cod_reservacion,fecha_inicio,fecha_fin,fecha_creacion,observacion,precio,cod_estado,cod_usuario,cod_cliente")] reservacion reservacion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.reservacion.Add(reservacion);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.cod_cliente = new SelectList(db.cliente, "cod_cliente", "nombres", reservacion.cod_cliente);
+            ViewBag.cod_estado = new SelectList(db.estado, "cod_estado", "nombre", reservacion.cod_estado);
+            ViewBag.cod_usuario = new SelectList(db.recepcionista, "cod_usuario", "nombres", reservacion.cod_usuario);
+            return View(reservacion);
+        }
+
+        // GET: Reservacion/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            reservacion reservacion = db.reservacion.Find(id);
+            if (reservacion == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.cod_cliente = new SelectList(db.cliente, "cod_cliente", "nombres", reservacion.cod_cliente);
+            ViewBag.cod_estado = new SelectList(db.estado, "cod_estado", "nombre", reservacion.cod_estado);
+            ViewBag.cod_usuario = new SelectList(db.recepcionista, "cod_usuario", "nombres", reservacion.cod_usuario);
+            return View(reservacion);
+        }
+
+        // POST: Reservacion/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "cod_reservacion,fecha_inicio,fecha_fin,fecha_creacion,observacion,precio,cod_estado,cod_usuario,cod_cliente")] reservacion reservacion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(reservacion).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.cod_cliente = new SelectList(db.cliente, "cod_cliente", "nombres", reservacion.cod_cliente);
+            ViewBag.cod_estado = new SelectList(db.estado, "cod_estado", "nombre", reservacion.cod_estado);
+            ViewBag.cod_usuario = new SelectList(db.recepcionista, "cod_usuario", "nombres", reservacion.cod_usuario);
+            return View(reservacion);
+        }
+
+        // GET: Reservacion/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            reservacion reservacion = db.reservacion.Find(id);
+            if (reservacion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(reservacion);
+        }
+
+        // POST: Reservacion/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            reservacion reservacion = db.reservacion.Find(id);
+            db.reservacion.Remove(reservacion);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
